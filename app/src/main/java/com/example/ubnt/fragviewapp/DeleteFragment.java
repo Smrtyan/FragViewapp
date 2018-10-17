5b4f78ccec210361d8c8f8babb729368f5dd094b
ubnt <547505786@qq.com>
2018-10-17 22:43:00 +0800

    1.加了个DeleteDialog,继承自DialogFragment，用法和fragment很相似，但是可以像Dialog一样弹出。在此fragment的布局文件就三个东西，一个TextView和两个按钮。 2. 在graddle中将版本从28改成27（DialogFragment在28过时了） 下一步，准备为按钮添加触发事件

--- app/src/main/java/com/example/ubnt/fragviewapp/DeleteFragment.java
+++ app/src/main/java/com/example/ubnt/fragviewapp/DeleteFragment.java
@@ -26,9 +26,9 @@ public class DeleteFragment extends DialogFragment {
         DeleteFragment f = new DeleteFragment();
 
         // Supply num input as an argument.
-        Bundle args = new Bundle();
-        args.putInt("num", num);
-        f.setArguments(args);
+//        Bundle args = new Bundle();
+//        args.putInt("num", num);
+//        f.setArguments(args);
 
         return f;
     }
@@ -36,37 +36,49 @@ public class DeleteFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mNum = getArguments().getInt("num");
+//        mNum = getArguments().getInt("num");
+//
+//        // Pick a style based on the num.
+//        int style = DialogFragment.STYLE_NORMAL, theme = 0;
+//        switch ((mNum-1)%6) {
+//            case 1: style = DialogFragment.STYLE_NO_TITLE; break;
+//            case 2: style = DialogFragment.STYLE_NO_FRAME; break;
+//            case 3: style = DialogFragment.STYLE_NO_INPUT; break;
+//            case 4: style = DialogFragment.STYLE_NORMAL; break;
+//            case 5: style = DialogFragment.STYLE_NORMAL; break;
+//            case 6: style = DialogFragment.STYLE_NO_TITLE; break;
+//            case 7: style = DialogFragment.STYLE_NO_FRAME; break;
+//            case 8: style = DialogFragment.STYLE_NORMAL; break;
+//        }
+//        switch ((mNum-1)%6) {
+//            case 4: theme = android.R.style.Theme_Holo; break;
+//            case 5: theme = android.R.style.Theme_Holo_Light_Dialog; break;
+//            case 6: theme = android.R.style.Theme_Holo_Light; break;
+//            case 7: theme = android.R.style.Theme_Holo_Light_Panel; break;
+//            case 8: theme = android.R.style.Theme_Holo_Light; break;
+//        }
+//        setStyle(style, theme);
 
-        // Pick a style based on the num.
-        int style = DialogFragment.STYLE_NORMAL, theme = 0;
-        switch ((mNum-1)%6) {
-            case 1: style = DialogFragment.STYLE_NO_TITLE; break;
-            case 2: style = DialogFragment.STYLE_NO_FRAME; break;
-            case 3: style = DialogFragment.STYLE_NO_INPUT; break;
-            case 4: style = DialogFragment.STYLE_NORMAL; break;
-            case 5: style = DialogFragment.STYLE_NORMAL; break;
-            case 6: style = DialogFragment.STYLE_NO_TITLE; break;
-            case 7: style = DialogFragment.STYLE_NO_FRAME; break;
-            case 8: style = DialogFragment.STYLE_NORMAL; break;
-        }
-        switch ((mNum-1)%6) {
-            case 4: theme = android.R.style.Theme_Holo; break;
-            case 5: theme = android.R.style.Theme_Holo_Light_Dialog; break;
-            case 6: theme = android.R.style.Theme_Holo_Light; break;
-            case 7: theme = android.R.style.Theme_Holo_Light_Panel; break;
-            case 8: theme = android.R.style.Theme_Holo_Light; break;
-        }
-        setStyle(style, theme);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_delete, container, false);
-        View tv = v.findViewById(R.id.textView);
+        Button btn_yes = v.findViewById(R.id.btn_yes);
+        Button btn_no = v.findViewById(R.id.btn_no);
+        btn_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
+            }
+        });
+        btn_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
         return v;
     }
 
